7b1053c8c2ca92ec11cdfee996e34b411038e7d4
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-09-24 00:21:03 +0400

    node

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-
+import React from 'react';
+import './style.css';
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
+          <h1>Today I Learned</h1>
+        </div>
+
+        <button className="btn-open btn btn-large">Share a fact</button>
+      </header>
+    </div>
+  )
 }
 
-export default App;
+export default App
