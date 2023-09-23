03fbc9921d8935e5a45b199c87570ecf2f40ef0f
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-09-24 00:43:04 +0400

    node

---
+++ src/NewFactForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewFactForm() {
+    return (
+        <>
+            <form className='fact-form'>Fact Form</form>
+        </>
+    )
+}
+
+export default NewFactForm
