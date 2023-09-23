193bb6645e49c7b682451f4ff3b232d5424e850a
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-09-24 02:37:05 +0400

    node modules

--- src/NewFactForm.jsx
+++ src/NewFactForm.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import './style.css';
 
+
+const CATEGORIES = [
+    { name: "technology", color: "#3b82f6" },
+    { name: "science", color: "#16a34a" },
+    { name: "finance", color: "#ef4444" },
+    { name: "society", color: "#eab308" },
+    { name: "entertainment", color: "#db2777" },
+    { name: "health", color: "#14b8a6" },
+    { name: "history", color: "#f97316" },
+    { name: "news", color: "#8b5cf6" },
+];
 function NewFactForm() {
 
     return (
@@ -11,9 +22,7 @@ function NewFactForm() {
             <input type="text" placeholder="Trustworthy source..." />
             <select>
                 <option value="">Choose category:</option>
-                <option value="technology">Technology</option>
-                <option value="science">Science</option>
-                <option value="finance">Finance</option>
+                {CATEGORIES.map((cat) => <option key={cat.name} value={cat.name}>{cat.name.toUpperCase()}</option>)}
             </select>
             <button className="btn btn-large">Post</button>
         </form>
