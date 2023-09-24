381e06c8483e2f484cbc96c762c263dd23d981b5
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-09-24 09:43:02 +0400

    node modules

--- src/NewFactForm.jsx
+++ src/NewFactForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css';
 
 
@@ -13,14 +13,27 @@ const CATEGORIES = [
     { name: "news", color: "#8b5cf6" },
 ];
 function NewFactForm() {
+    const [text, setText] = useState("");
+    const [source, setSource] = useState("");
+    const [category, setcategory] = useState("");
+    const textLength = text.length;
+
+    function handleSubmit(e) {
+        //1. Prevent browser reload
+        e.preventDefault();
+
+        console.log(text, source, category);
+
+    }
 
     return (
 
-        <form className="formEl fact-form">
-            <input type="text" placeholder="Share a fact with the world..." />
-            <span>200</span>
-            <input type="text" placeholder="Trustworthy source..." />
-            <select>
+        <form className="formEl fact-form" onSubmit={handleSubmit}>
+            <input type="text" placeholder="Share a fact with the world..." value={text}
+                onChange={(e) => setText(e.target.value)} />
+            <span>{200 - textLength}</span>
+            <input type="text" value={source} onChange={(e) => setSource(e.target.value)} placeholder="Trustworthy source..." />
+            <select value={category} onChange={(e) => setcategory(e.target.value)}>
                 <option value="">Choose category:</option>
                 {CATEGORIES.map((cat) => <option key={cat.name} value={cat.name}>{cat.name.toUpperCase()}</option>)}
             </select>
